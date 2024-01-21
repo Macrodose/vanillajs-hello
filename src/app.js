7152726fc678d2c8540bccf92533ae43abf78ecd
Macrodose <122926023+Macrodose@users.noreply.github.com>
2024-01-21 14:37:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  return "My dog ate my homework";
 };
