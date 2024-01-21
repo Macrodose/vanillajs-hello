033f1c09fa28f6ac9e352fb24dacbaf95b2518a8
Macrodose <122926023+Macrodose@users.noreply.github.com>
2024-01-21 19:23:42 +0000

    2nd commit

--- src/app.js
+++ src/app.js
@@ -12,5 +12,44 @@ window.onload = () => {
 };
 
 let generateExcuse = () => {
-  return "My dog ate my homework";
+  let pronoun = ["My", "Your"];
+  let subject = [
+    "doctor",
+    "dealer",
+    "assistant",
+    "probation officer",
+    "soulmate"
+  ];
+  let verb = [
+    "vaporised my",
+    "swallowed my",
+    "made fun of my",
+    "played with my"
+  ];
+  let object = ["heart", "innocence", "outfit", "sanity", "memories"];
+  let place = [
+    "at the strip club",
+    "at KFC",
+    "at the Fridays for Future meeting",
+    "at the junkyard",
+    "at the knitting class"
+  ];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let verbIndex = Math.floor(Math.random() * verb.length);
+  let objectIndex = Math.floor(Math.random() * object.length);
+  let placeIndex = Math.floor(Math.random() * place.length);
+
+  return (
+    pronoun[pronounIndex] +
+    "" +
+    subect[subjectIndex] +
+    "" +
+    verb[verbIndex] +
+    "" +
+    object[objectIndex] +
+    "" +
+    place[placeIndex]
+  );
 };
